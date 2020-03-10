c29c0944e1d5a6538ec2f35a8be218c0f323cd16
Denmlak <55392010+Denmlak@users.noreply.github.com>
2020-03-10 21:55:35 +0100

    uploading

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app= express();
 const server= require('http').Server(app);
 const io= require('socket.io')(server);
-const port= 3000 || process.env.PORT;
+const port=process.env.PORT || 3000;
 let users= {};
 
 app.use(express.static('public'));
